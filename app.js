a0fde6d76a7181aa09663ef936f6cf1b2379c17d
ernestbies <ernestbies@gmail.com>
2020-05-29 14:18:00 +0200

    add server

--- app.js
+++ app.js
@@ -30,6 +30,12 @@ process.on('SIGINT', () => {
   });
 });
 
+
+app.get('/*', function (req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+
 routes(app);
 app.listen(config.port, () => {
   console.info(`Server is running at ${config.port}`)
